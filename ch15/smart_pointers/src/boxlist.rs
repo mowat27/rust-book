bf70f0a770418cfc54636b53e10deba52e73344c
Adrian Mowat <adrian.mowat@gmail.com>
2020-07-07 19:24:34 +0100

    Print the cons list

--- ch15/smart_pointers/src/boxlist.rs
+++ ch15/smart_pointers/src/boxlist.rs
@@ -1,10 +1,27 @@
-enum List {
-  Cons(i32, Box<List>),
+enum List<T> {
+  Cons(T, Box<List<T>>),
   Nil,
 }
 
 use List::{Cons, Nil};
 
 pub fn run() {
-  let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+  let b = Box::new(23);
+  print!("b has {} in the box\n", b);
+
+  print!("Printing the list\n");
+  let list = Cons(
+    "Hello",
+    Box::new(Cons("from", Box::new(Cons("boxes", Box::new(Nil))))),
+  );
+  let mut item = list;
+  loop {
+    match item {
+      Cons(val, next) => {
+        println!("  {}", val);
+        item = *next;
+      }
+      Nil => break,
+    }
+  }
 }
